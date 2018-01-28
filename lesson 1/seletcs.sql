196137cc9121684e67ae20b79c874034b50c1e85
RudzinskyiYurii <rudzinskyi.yurii@gmail.com>
2018-01-28 15:55:11 +0200

    add smth new

--- lesson 1/seletcs.sql
+++ lesson 1/seletcs.sql
@@ -29,6 +29,48 @@ ORDER BY type DESC*/
 	WHERE price BETWEEN 400 AND 600
 ORDER BY hd*/
 
-SELECT type.model, type.speed, type.hd FROM Labor_SQL.Product
-	WHERE (type.hd = '10' OR type.hd = '20') AND maker = 'A'
-ORDER BY type.speed
+/*
+SELECT model, speed, hd FROM Labor_SQL.PC
+	WHERE hd IN (10, 20) AND model IN (SELECT model FROM Labor_SQL.Product
+		WHERE maker = 'A')
+ORDER BY speed*/
+
+/*SELECT model, speed, hd, price FROM Labor_SQL.Laptop
+WHERE screen >= 12
+ORDER BY price DESC */
+
+-- 2 task
+
+/*SELECT * FROM Labor_SQL.PC
+	WHERE model RLIKE '[1]{2,}'*/
+    
+/*SELECT * FROM Labor_SQL.Outcome
+WHERE month(date) = '03'*/
+
+/*SELECT * FROM Labor_SQL.Outcome_o
+WHERE day(date) = '14'*/
+
+/*SELECT name FROM Labor_SQL.Ships
+WHERE Ships.name RLIKE '^W' AND name RLIKE 'n$' */
+
+/*SELECT name FROM Labor_SQL.Ships
+WHERE name RLIKE '[e]{2}'*/
+
+-- task 3
+
+/*SELECT DISTINCT Product.maker, type, speed, hd FROM Labor_SQL.Product, Labor_SQL.PC
+WHERE Product.model = PC.model AND hd <=8*/
+
+/*SELECT maker FROM Labor_SQL.Product, Labor_SQL.PC
+WHERE Product.model = PC.model AND speed >= 600*/
+
+/*SELECT maker FROM Labor_SQL.Product, Labor_SQL.Laptop
+WHERE Product.model = Laptop.model AND speed <= 500*/
+
+/*SELECT Laptop1.model, Laptop2.model, Laptop1.hd, Laptop1.ram FROM Labor_SQL.Laptop AS Laptop1
+INNER JOIN Labor_SQL.Laptop AS Laptop2 ON (Laptop1.hd = Laptop2.hd 
+                  AND Laptop1.ram = Laptop2.ram
+                  AND Laptop1.model < Laptop2.model)
+ORDER BY Laptop1.model DESC*/
+
+
